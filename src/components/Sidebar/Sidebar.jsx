b0c6982b3b821108c60cc927f4686acf3327f650
Donny Pangiliners <oshogautamamahavirzaldua@gmail.com>
2024-10-09 15:34:34 +0800

    Use external file svg for svg icons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,15 @@
 import './Sidebar.css';
+import { default as moneyIcon} from './money.svg'
+import { default as dashboardIcon } from './dashboard.svg'
 
 const Sidebar = ({}) => {
     return (
       <nav className="sidebar">
         <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          width="36px"
-          height="36px"
-        >
-          <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8v-10h-8v10zM13 3v6h8V3h-8z" />
-        </svg>
+          <img src={dashboardIcon} />
         </button>
         <button>
-          Rawr
+          <img src={moneyIcon} />
         </button>
         <button>
           Rawr
