be3bfa747f0508362e51f9006beba4a13574c8cb
Donny Pangiliners <oshogautamamahavirzaldua@gmail.com>
2024-10-11 11:40:41 +0800

    Make the dashboard icon into a react component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,11 @@
 import './Sidebar.css';
+import DashboardIcon from './DashboardIcon';
 
 const Sidebar = ({}) => {
     return (
       <nav className="sidebar">
         <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8v-10h-8v10zM13 3v6h8V3h-8z" />
-        </svg>
+        <DashboardIcon />
         </button>
         <button>
           <svg
