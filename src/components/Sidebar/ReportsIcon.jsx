609dcd003c31981268eceecd0a32d2233ceec252
Donny Pangiliners <oshogautamamahavirzaldua@gmail.com>
2024-10-14 15:47:19 +0800

    Transfer all icons in the sidebar to components

---
+++ src/components/Sidebar/ReportsIcon.jsx
@@ -0,0 +1,14 @@
+const ReportsIcon = ({}) => {
+    return (
+        <svg
+            className="icons"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+          </svg>
+    )
+}
+
+export default ReportsIcon;
