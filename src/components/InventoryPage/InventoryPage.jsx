d29cdd2b05b9754b7f39d877f9a8b907e422c4ad
Donny Pangiliners <oshogautamamahavirzaldua@gmail.com>
2024-10-21 10:01:50 +0800

    Add tabs for InventoryPage

--- src/components/InventoryPage/InventoryPage.jsx
+++ src/components/InventoryPage/InventoryPage.jsx
@@ -1,14 +1,36 @@
 import './InventoryPage.css';
 import InventoryIcon from './InventoryIcon';
+import ListIcon from './ListIcon';
+import CheckIcon from './CheckIcon';
+import PaperIcon from './PaperIcon';
+import WrongIcon from './WrongIcon';
 
 const InventoryPage = ({}) => {
     return (
       <>
-          <div class="title">
+          <div className='title'>
             <InventoryIcon />
             <p>
                 Inventory
             </p>
+          </div>
+          <div className='tabs'>
+            <div className='tab'>
+              <ListIcon />
+              Inventory
+            </div>
+            <div className='tab'>
+              <CheckIcon />
+              Executed
+            </div>
+            <div className='tab'>
+              <PaperIcon />
+              Draft
+            </div>
+            <div className='tab'>
+              <WrongIcon />
+              Cancelled
+            </div>
           </div>
       </>
     );
