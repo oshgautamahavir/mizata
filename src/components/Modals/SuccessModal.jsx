265b028075a990617aaf5a895051dca075999915
Donny Pangiliners <oshogautamamahavirzaldua@gmail.com>
2024-11-25 14:49:14 +0800

    Adjust css of SuccessModal

--- src/components/Modals/SuccessModal.jsx
+++ src/components/Modals/SuccessModal.jsx
@@ -1,18 +1,22 @@
 import PropTypes from 'prop-types'
 
+import './SuccessModal.css';
+
+import CircleCheckIcon from './CircleCheckIcon';
+
 const SuccessModal = ({ showSuccessModal, closeModal }) => {
   if (!showSuccessModal) return null;
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
+      <div className="success-modal">
         <div className="icon">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="green" className="w-64 h-32 ml-8 mt-8">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-          </svg>
+          <CircleCheckIcon />
         </div>
-        <p className="success-modal-message"> Item has been successfully created. </p>
-        <button className="dismiss-button" onClick={closeModal}>Dismiss</button>
+        <p > Item has been successfully created. </p>
+        <div className='button-container'>
+          <button onClick={closeModal}> Dismiss </button>
+        </div>
       </div>
     </div>
   );
